9bdc34aba3e632a70dc93ee338f79def226e1919
john <john.palli@gmail.com>
2017-04-04 16:06:56 +0000

    added motd and changed apache default.rb for node attributes

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,4 +6,5 @@ log_location             STDOUT
 node_name                "john"
 client_key               "#{current_dir}/john.pem"
 chef_server_url          "https://johnpalli2.mylabserver.com:443/organizations/linuxacademy"
-cookbook_path            ["#{current_dir}/../cookbooks"]
+#cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_path            ["/home/user/chef-repo/cookbooks"]
