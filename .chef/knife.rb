53d86921dfb93d6b900ee7bd16fa0b34034b618a
john <john.palli@gmail.com>
2017-04-04 14:04:12 +0000

    add basic apache cookbook

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,5 +5,5 @@ log_level                :info
 log_location             STDOUT
 node_name                "john"
 client_key               "#{current_dir}/john.pem"
-chef_server_url          "https://johnpalli2.mylabserver.com/organizations/linuxacademy"
+chef_server_url          "https://johnpalli2.mylabserver.com:443/organizations/linuxacademy"
 cookbook_path            ["#{current_dir}/../cookbooks"]
